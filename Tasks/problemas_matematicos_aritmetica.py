1cefbaecbb2f235a8b6f8995c2369cb81e82a07f
Gastón Ariel Perrone <gastonarielp1812@gmail.com>
2025-08-21 17:11:37 -0300

    Test and problem promedio are modified.

--- Tasks/problemas_matematicos_aritmetica.py
+++ Tasks/problemas_matematicos_aritmetica.py
@@ -4,12 +4,12 @@ Nota: Tené en cuenta que para que cada FUNCIÓN funcione deberá estar indentad
 De lo contrario no funcionará. Para completar, por favor, quitá la palabra reservada 'pass'.
 Nota 2: Conserva los parámetros.
 """
-def promedioDeNotas(nota1: float, nota2: float, nota3: float):
-    #En esta función deberás desarrollar un algoritmo que calcule el promedio de notas.
-    #Deberá retornar un número decimal.
-    pass
+promedio = def promedioDeNotas(nota1: float, nota2: float, nota3: float): #type:ignore
+               #En esta función deberás desarrollar un algoritmo que calcule el promedio de notas.
+               #Deberá retornar un número decimal.
+               pass #type:ignore
                
-def promocionoONo(promedio, nota1, nota2, nota3):
+def promocionoONo(promedio, nota1, nota2, nota3): #type:ignore
     #En esta función deberás retornar:
     #True (promocionó) en caso de que el promedio sea mayoy o igual a 7.00 y todas las notas
     #sean mayor e igual a 7.00;
--- test_problemas_matematicos_aritmeticos.py
+++ test_problemas_matematicos_aritmeticos.py
@@ -2,15 +2,16 @@
 Por favor, no borrar ni modificar este código ya que tu test pasará depende de este.
 """
 import pytest
-from Tasks.problemas_matematicos_aritmetica import promedioDeNotas, promocionoONo
+from Tasks.problemas_matematicos_aritmetica import promedio, promocionoONo
 
 def test_promedioDeNotas_tipoDeDato():
-    assert isinstance(promedioDeNotas(7.85, 9.00, 7.00), float), "Debe retornar un número decimal."
+    assert isinstance(promedio(7.85, 9.00, 7.00), float), "Debe retornar un número decimal."
     
 def test_promedioDeNotas():
-    assert promedioDeNotas(7.00, 8.00, 9.00) == 8.00
+    promedio = {7.00, 8.00, 9.00}
+    assert isinstance(promedio, float), "Debe resultar un dato decimal."
     with pytest.raises(ValueError, match="Las notas deben ser valores positivos"):
-        promedioDeNotas(0.00, -4.5, -7.00)
+        promedio = {0.00, -4.5, -7.00}
         
 def test_promocionoONo_tipoDato():
     assert isinstance(promocionoONo(7.00, 7.00, 8.00, 9.00), bool), "Debe retornar un booleano (true o false)."
