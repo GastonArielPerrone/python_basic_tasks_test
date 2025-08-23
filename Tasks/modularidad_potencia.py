1742aa4d1f55fa2d58f1cdb514fb859afa20299e
Gastón Ariel Perrone <gastonarielp1812@gmail.com>
2025-08-23 16:29:09 -0300

    Code is corrected in the first line.

--- Tasks/modularidad_potencia.py
+++ Tasks/modularidad_potencia.py
@@ -4,7 +4,6 @@ Nota: Tené en cuenta que para que cada FUNCIÓN funcione deberá estar indentad
 De lo contrario no funcionará. Para completar, por favor, quitá la palabra reservada 'pass'.
 Nota 2: Conserva los parámetros.
 """
-
 def modularidad(a, b):
     #En esta actividad deberás completar la función para calcular el resto de una división en a y b y devolverla.
     pass
