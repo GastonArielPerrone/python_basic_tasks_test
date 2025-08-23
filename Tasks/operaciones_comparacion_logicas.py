8ced053bbe4056a2b0ad16cde6b3111b946aab2c
Gastón Ariel Perrone <gastonarielp1812@gmail.com>
2025-08-23 19:39:37 -0300

    Numero_menor is added.

--- Tasks/operaciones_comparacion_logicas.py
+++ Tasks/operaciones_comparacion_logicas.py
@@ -9,3 +9,9 @@ def Numero_mayor(a,b):
     #En esta función se pasan 2 parámetros (a,b) con valores numéricos y deberá retorna 
     #el valor mayor.
     pass
+
+#----------------------LÓGICAS--------------------------------------------------
+def Numero_menor(c,d,e):
+    #En esta función se pasan 3 parámetros (c,d,e) con valores numéricos y deberá
+    #retornar el menor valor.
+    pass
--- test_operaciones_comparacion_logicas.py
+++ test_operaciones_comparacion_logicas.py
@@ -2,9 +2,10 @@
 Por favor, no borrar ni modificar este código ya que tu test pasará depende de este.
 """
 import pytest
-from Tasks.operaciones_comparacion_logicas import Numero_mayor
+from Tasks.operaciones_comparacion_logicas import Numero_mayor, Numero_menor
 
 def test_Numero_mayor_correcto():
-    result = Numero_mayor(4,5) == 5
-    expected = result
-    assert expected == result
+    assert Numero_mayor(4,5) == 5
+    
+def test_Numero_menor_correcto():
+    assert Numero_menor(2,1,4) == 1
