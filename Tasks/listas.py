87a68cbd747256104fd397a94b9e967a38c98f17
Gastón Ariel Perrone <gastonarielp1812@gmail.com>
2025-08-28 13:49:16 -0300

    Exercises and test about listas are added.

---
+++ Tasks/listas.py
@@ -0,0 +1,38 @@
+"""
+LISTAS
+Nota: Tené en cuenta que para que cada FUNCIÓN funcione deberá estar indentada correctamente.
+De lo contrario no funcionará. Para completar, por favor, quitá la palabra reservada 'pass'.
+Nota 2: Conserva los parámetros.
+"""
+
+from itertools import count
+
+
+def Agregar_al_carrito(productos: list, producto):
+    #En esta función tenemos un carrito vació donde necesitamos agregarle "productos".
+    #En este caso vamos a agregar un solo producto. Debes retornar la lista con el producto
+    #agregado.
+    pass
+
+def Agregar_otro_producto_al_carrito(productos: list, producto):
+    #En esta función ya están agregados algunos productos en nuestro carrito de compras.
+    #Agregue otro producto al carrito de compras.
+    pass
+
+def Contar_productos_adentro_del_carrito(productos: list):
+    #En esta función deberás retornar la cantidad de productos que hay adentro
+    #del carrito.
+    #Pista: Investigue la funcionalidad de len().
+    pass
+
+def Quitar_productos_al_carrito(productos: list):
+    #En esta función teniendo el carrito lleno de productos eliminaremos el último
+    #pruducto agregado y retornar la lista de "productos".
+    pass
+
+def Numero_esta_en_la_lista(lista: list, numero):
+    #En esta función deberás consultar si el número indicado por parámetro se
+    #encuentra en la lista de números.
+    #En caso de que dicho número se encuentre retornará True; caso contrario, False.
+    #Podrás usar "for" loop o "in". Investiga ambas funciones.
+    pass
---
+++ test_listas.py
@@ -0,0 +1,48 @@
+import pytest
+from Tasks.listas import Agregar_al_carrito, Agregar_otro_producto_al_carrito, Contar_productos_adentro_del_carrito, Quitar_productos_al_carrito, Numero_esta_en_la_lista
+
+def test_Agregar_al_carrito_tipoDato():
+    assert isinstance(Agregar_al_carrito([], "Agua"), list), "Debe retornar una lista."
+    
+def test_Agregar_al_carrito_correcto():
+    productos = []
+    producto = "Agua"
+    resultado_esperado = ["Agua"]
+    
+    lista_final = Agregar_al_carrito(productos, producto)
+    assert lista_final == resultado_esperado
+    
+def test_Agregar_otro_producto_al_carrito_de_compras_tipoDato():
+    assert isinstance(Agregar_otro_producto_al_carrito([], "Agua"), list), "Debe retornar una lista."
+    
+def test_Agregar_otro_producto_al_carrito_de_compras_correcto():
+    productos = ["Agua"]
+    producto = ["Lechuga"]
+    resultado_esperado = ["Agua", "Lechuga"]
+    
+    lista_final = Agregar_otro_producto_al_carrito(productos, producto)
+    assert lista_final == resultado_esperado
+    
+def test_Contar_productos_adentro_del_carrito_tipoDato():
+    assert isinstance(Contar_productos_adentro_del_carrito(["Agua", "Lechuga"]), int), "Debe retornar un número entero."
+    
+def test_Contar_productos_adentro_del_carrito_correcto():
+    assert Contar_productos_adentro_del_carrito(["Agua", "Lechuga"]) == 2
+    assert Contar_productos_adentro_del_carrito([]) == 0
+    
+def test_Quitar_productos_al_carrito_tipoDato():
+    assert isinstance(Quitar_productos_al_carrito(["Agua", "Lechuga"]), list), "Debe retornar una lista."
+    
+def test_Quitar_productos_al_carrito_correcto():
+    productos = ["Agua", "Lechuga"]
+    resultado_esperado = ["Agua"]
+    
+    productos_final = Quitar_productos_al_carrito(productos)
+    assert productos_final == resultado_esperado
+    
+def test_Numero_esta_en_la_lista_tipoDato():
+    assert isinstance(Numero_esta_en_la_lista([1,2,3,4,5], 5), bool), "Debe retornar un booleano."
+    
+def test_Numero_esta_en_la_lista_correcto():
+    assert Numero_esta_en_la_lista([1,2,3,4], 4) == True
+    assert Numero_esta_en_la_lista([1,2,3,4], 5) == False
